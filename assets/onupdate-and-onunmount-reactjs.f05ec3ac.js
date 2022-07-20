fce15dc60d3267ab7982d1cd6e98fde29a60d2fb
tachibana-shin <45375496+tachibana-shin@users.noreply.github.com>
2022-07-20 09:54:04 +0000

    Deploying to gh-pages from @ tachibana-shin/tachibana-shin.github.io@abe2e0e861f02e02807dd007b57487bfabf2cd8d 🚀

---
+++ assets/onupdate-and-onunmount-reactjs.f05ec3ac.js
@@ -0,0 +1,25 @@
+import{_ as e}from"./Post.a0757671.js";import{u as c,c as p,w as u,o as l,a as n,d as t}from"./app.2d4fc991.js";const k=n("div",{class:"prose m-auto"},[n("p",null,[n("code",null,"onUpdate"),t(" in Reactjs hooks:")]),n("pre",{class:"language-tsx"},[n("code",{class:"language-tsx"},[n("span",{class:"token keyword"},"import"),t(),n("span",{class:"token punctuation"},"{"),t(" useEffect "),n("span",{class:"token punctuation"},"}"),t(),n("span",{class:"token keyword"},"from"),t(),n("span",{class:"token string"},'"react"'),t(`
+
+`),n("span",{class:"token keyword"},"function"),t(),n("span",{class:"token function"},"App"),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},")"),t(),n("span",{class:"token punctuation"},"{"),t(`
+  `),n("span",{class:"token function"},"useEffect"),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},")"),t(),n("span",{class:"token operator"},"=>"),t(),n("span",{class:"token punctuation"},"{"),t(`
+    `),n("span",{class:"token comment"},"// on mount"),t(`
+    `),n("span",{class:"token keyword"},"return"),t(),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},")"),t(),n("span",{class:"token operator"},"=>"),t(),n("span",{class:"token punctuation"},"{"),t(`
+      `),n("span",{class:"token comment"},"// call if component update"),t(`
+    `),n("span",{class:"token punctuation"},"}"),t(`
+  `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},")"),t(`
+
+  `),n("span",{class:"token keyword"},"return"),t(),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"<"),t("div")]),n("span",{class:"token punctuation"},">")]),n("span",{class:"token plain-text"},"App"),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"</"),t("div")]),n("span",{class:"token punctuation"},">")]),t(`
+`),n("span",{class:"token punctuation"},"}"),t(`
+`)])]),n("p",null,[n("code",null,"onUnmount"),t(" in Reactjs hooks:")]),n("pre",{class:"language-tsx"},[n("code",{class:"language-tsx"},[n("span",{class:"token keyword"},"import"),t(),n("span",{class:"token punctuation"},"{"),t(" useEffect "),n("span",{class:"token punctuation"},"}"),t(),n("span",{class:"token keyword"},"from"),t(),n("span",{class:"token string"},'"react"'),t(`
+
+`),n("span",{class:"token keyword"},"function"),t(),n("span",{class:"token function"},"App"),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},")"),t(),n("span",{class:"token punctuation"},"{"),t(`
+  `),n("span",{class:"token function"},"useEffect"),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},")"),t(),n("span",{class:"token operator"},"=>"),t(),n("span",{class:"token punctuation"},"{"),t(`
+    `),n("span",{class:"token comment"},"// on mount"),t(`
+    `),n("span",{class:"token keyword"},"return"),t(),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},")"),t(),n("span",{class:"token operator"},"=>"),t(),n("span",{class:"token punctuation"},"{"),t(`
+      `),n("span",{class:"token comment"},"// call if component unmount"),t(`
+    `),n("span",{class:"token punctuation"},"}"),t(`
+  `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},","),t(),n("span",{class:"token punctuation"},"["),n("span",{class:"token punctuation"},"]"),n("span",{class:"token punctuation"},")"),t(`
+
+  `),n("span",{class:"token keyword"},"return"),t(),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"<"),t("div")]),n("span",{class:"token punctuation"},">")]),n("span",{class:"token plain-text"},"App"),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"</"),t("div")]),n("span",{class:"token punctuation"},">")]),t(`
+`),n("span",{class:"token punctuation"},"}"),t(`
+`)])])],-1),_="onUpdate and onUnmount in ReactJS hooks.",U=[{property:"og:title",content:"onUpdate and onUnmount in ReactJS hooks."}],h={__name:"onupdate-and-onunmount-reactjs",setup(i,{expose:a}){const s={title:"onUpdate and onUnmount in ReactJS hooks.",meta:[{property:"og:title",content:"onUpdate and onUnmount in ReactJS hooks."}]};return a({frontmatter:s}),c({title:"onUpdate and onUnmount in ReactJS hooks.",meta:[{property:"og:title",content:"onUpdate and onUnmount in ReactJS hooks."}]}),(d,m)=>{const o=e;return l(),p(o,{frontmatter:s},{default:u(()=>[k]),_:1})}}};export{h as default,U as meta,_ as title};
